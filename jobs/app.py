0c9496c35397abb25219554c7a5012ff221a7d92
Ronald Eddings <ronaldeddings@gmail.com>
2018-10-22 08:59:25 -0700

    Show all jobs
    Not currenlty working as expected

--- jobs/app.py
+++ jobs/app.py
@@ -6,12 +6,14 @@ PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 
+
 def open_connection():
     connection = getattr(g, "_connection", None)
     if not connection:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -24,13 +26,16 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, "connection", None)
     if connection is not None:
         connection.close()
 
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template("index.html")
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template("index.html", jobs=jobs)
